3f94a68b11700d229f5c46544999871259e07b8e
MdSamreAlam <mohammedsamre.alam@augmentolabs.com>
2020-12-10 15:29:48 +0530

    Testing
    Compound interest calculation

---
+++ CompoundInterest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompoundInterest
+{
+    /* 
+ COPY RIGHT @AUGMENTO LABS 2020
+Created By Alam
+*/
+    public class CompoundInterest
+    {
+        public double CalculateCompoundInterest(double amount, double interest, int years, int annualfrequency)
+        {
+            double balance;
+
+            // Formula use to calculate compound interest
+            balance = amount * Math.Pow((1 + interest / annualfrequency), years * annualfrequency);
+            return balance;
+
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CompoundInterest
+{
+    /* 
+ COPY RIGHT @AUGMENTO LABS 2020
+Created By Alam
+*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount;
+            double interest;
+            int annualcompound;
+            int years;
+            // double balance = 0;
+
+            Console.Write("Enter the Amount:");
+            amount = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the Interest Rate:");
+            interest = Convert.ToDouble(Console.ReadLine()) / 100;
+
+            Console.Write("Enter number of Years:");
+            years = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter annual frequency:");
+            annualcompound = Convert.ToInt32(Console.ReadLine());
+            CompoundInterest compoundIntrest = new CompoundInterest();
+            var results = compoundIntrest.CalculateCompoundInterest(amount, interest, years, annualcompound);
+
+            Console.WriteLine("Total Amount Balance is: {0}", results);
+
+            Console.ReadLine();
+        }
+    }
+ }
+
